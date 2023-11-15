2d2511a8bec6c2bf764c52e715a7aff114429cf4
celyes <celyes02@gmail.com>
2023-11-15 15:54:45 +0100

    Add success method to controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function success(string|array $data, $code = 200): JsonResponse
+    {
+        $response = ['success' => true];
+
+        if (is_array($data)) {
+            $response += $data;
+        } else {
+            $response['message'] = $data;
+        }
+
+        return response()->json($response, $code);
+    }
 }
